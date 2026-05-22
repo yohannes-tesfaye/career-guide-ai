962f552f747199482def840690f0f2f6eb4fc2b9
yohannes <yohannesgoat@gmail.com>
2026-05-22 03:23:59 +0300

    fix: import update

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import { PrismaClient } from "../generated/prisma/client";
+import { PrismaClient } from "@/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
